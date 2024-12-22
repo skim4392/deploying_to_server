d3099e24d4a10537c56adef0c5a6002aebefb4dc
Shikhar Mittal <shikharmittal@mac.mynetworksettings.com>
2024-12-21 21:09:06 -0500

    Initial Commit. Ready To Deploy

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify({"message": "Hello Render!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
